0ec622ad1b5e7c9efc2a4136059adfd8b380fbb0
hilal555 <muhamadhilal04@gmail.com>
2024-12-15 18:21:00 +0700

    membuat fungsi HomeMhsView

--- app/src/main/java/com/example/roomsiswa/ui/view/mahasiswa/HomeMhsView.kt
+++ app/src/main/java/com/example/roomsiswa/ui/view/mahasiswa/HomeMhsView.kt
@@ -7,21 +7,72 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.DateRange
 import androidx.compose.material.icons.filled.Home
 import androidx.compose.material.icons.filled.Person
 import androidx.compose.material3.Card
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.roomsiswa.data.entity.Mahasiswa
+import com.example.roomsiswa.ui.viewmodel.HomeMhsViewModel
+import com.example.roomsiswa.ui.viewmodel.PenyediaViewModel
+
+@Composable
+fun HomeMhsView(
+    viewModel: HomeMhsViewModel = viewModel(factory = PenyediaViewModel.Factory),
+    onAddMhs: () -> Unit = { },
+    onDetailClick: (String) -> Unit = { },
+    modifier: Modifier = Modifier
+){
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                judul = "Daftar Mahasiswa",
+                showBackButton = false,
+                onBack = { },
+                modifier = modifier
+            )
+        },
+        floatingActionButton = {
+         FloatingActionButton(
+             onClick = onAddMhs,
+             shape = MaterialTheme.shapes.medium,
+             modifier = Modifier.padding(16.dp)
+         ) {
+             Icon(
+                 imageVector = Icons.Default.Add,
+                 contentDescription = "Tambah Mahasiswa"
+             )
+         }
+        }
+    ) { innerPadding ->
+        val homeUiState by viewModel.homeUiState.collectAsState()
+
+        BodyHomeMhsView(
+            homeUiState = homeUiState,
+            onClick = {
+                onDetailClick(it)
+            },
+            modifier = Modifier.padding(innerPadding)
+        )
+
+    }
+}
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
