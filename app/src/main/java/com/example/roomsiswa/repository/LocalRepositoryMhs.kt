6635de3c90733c1508987cb464dfa19017819350
hilal555 <muhamadhilal04@gmail.com>
2024-12-15 13:55:51 +0700

    memanggil fungsi getAllMahasiswa dari MahasiswaDao untuk mendapatkan data mahasiswa

--- app/src/main/java/com/example/roomsiswa/repository/LocalRepositoryMhs.kt
+++ app/src/main/java/com/example/roomsiswa/repository/LocalRepositoryMhs.kt
@@ -2,6 +2,7 @@ package com.example.roomsiswa.repository
 
 import com.example.roomsiswa.data.dao.MahasiswaDao
 import com.example.roomsiswa.data.entity.Mahasiswa
+import kotlinx.coroutines.flow.Flow
 
 class LocalRepositoryMhs
     (private val mahasiswaDao: MahasiswaDao)  : RepositoryMhs
@@ -10,4 +11,9 @@ class LocalRepositoryMhs
         mahasiswaDao.insertMahasiswa(mahasiswa)
     }
 
+    override fun getAllMhs(): Flow<List<Mahasiswa>> {
+        return mahasiswaDao.getAllMahasiswa()
+    }
+
 }
+
