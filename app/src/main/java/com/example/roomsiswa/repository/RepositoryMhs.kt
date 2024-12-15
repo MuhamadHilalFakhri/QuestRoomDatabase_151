a6239d19fe84a4bb8fc97b47e4dcc71e8d6c8205
hilal555 <muhamadhilal04@gmail.com>
2024-12-15 13:41:47 +0700

    memanggil fungsi getAllMahasiswa dari MahasiswaDao

--- app/src/main/java/com/example/roomsiswa/repository/RepositoryMhs.kt
+++ app/src/main/java/com/example/roomsiswa/repository/RepositoryMhs.kt
@@ -1,8 +1,11 @@
 package com.example.roomsiswa.repository
 
 import com.example.roomsiswa.data.entity.Mahasiswa
+import kotlinx.coroutines.flow.Flow
 
 interface RepositoryMhs {
 
     suspend fun insertMhs(mahasiswa: Mahasiswa)
+
+    fun getAllMhs(): Flow<List<Mahasiswa>>
 }
