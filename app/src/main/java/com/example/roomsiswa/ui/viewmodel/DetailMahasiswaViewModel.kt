fa1a0e662667845d7aec0b466699428402977860
hilal555 <muhamadhilal04@gmail.com>
2024-12-15 15:06:46 +0700

    membuat fungsi untuk memindahkan data dari entity ke ui

--- app/src/main/java/com/example/roomsiswa/ui/viewmodel/DetailMahasiswaViewModel.kt
+++ app/src/main/java/com/example/roomsiswa/ui/viewmodel/DetailMahasiswaViewModel.kt
@@ -1,11 +1,31 @@
 package com.example.roomsiswa.ui.viewmodel
 
+import com.example.roomsiswa.data.entity.Mahasiswa
 import com.example.roomsiswa.ui.viewmodel.MahasiswaViewModel.MahasiswaEvent
-import java.lang.Error
+
 
 data class DetailUiState(
     val detailUiEvent: MahasiswaEvent = MahasiswaEvent(),
     val isLoading: Boolean = false,
     val isError: Boolean = false,
     val errorMessage: String = ""
-)
+) {
+    val isUiEventEmpty: Boolean
+        get() = detailUiEvent == MahasiswaEvent()
+
+    val isUiEventNotEmpty: Boolean
+        get() = detailUiEvent != MahasiswaEvent()
+}
+
+//Memindahkan data dari entity ke ui
+fun Mahasiswa.toDetailUiEvent(): MahasiswaEvent{
+    return MahasiswaEvent(
+        nim = nim,
+        nama = nama,
+        jenisKelamin = jenisKelamin,
+        alamat = alamat,
+        kelas = kelas,
+        angkatan = angkatan
+    )
+}
+
