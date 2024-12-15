f8ee868efd8fe79435c3f383fbd9a1d4037fff96
hilal555 <muhamadhilal04@gmail.com>
2024-12-15 14:11:49 +0700

    membuat class HomeMhsViewModel

--- app/src/main/java/com/example/roomsiswa/ui/viewmodel/HomeMhsViewModel.kt
+++ app/src/main/java/com/example/roomsiswa/ui/viewmodel/HomeMhsViewModel.kt
@@ -1,6 +1,15 @@
 package com.example.roomsiswa.ui.viewmodel
 
+import androidx.lifecycle.ViewModel
 import com.example.roomsiswa.data.entity.Mahasiswa
+import com.example.roomsiswa.repository.LocalRepositoryMhs
+import com.example.roomsiswa.repository.RepositoryMhs
+
+class HomeMhsViewModel (
+    private val repositoryMhs: RepositoryMhs
+) : ViewModel (){
+
+}
 
 data class HomeUiSate(
     val listMhs: List<Mahasiswa> = listOf(),
