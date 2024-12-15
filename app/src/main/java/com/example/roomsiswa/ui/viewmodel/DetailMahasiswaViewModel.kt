29257f102b8ff15e2716ada0c5cccab8e2e6d4ba
hilal555 <muhamadhilal04@gmail.com>
2024-12-15 15:03:21 +0700

    menambahkan variabel ke data class DetailUiState

--- app/src/main/java/com/example/roomsiswa/ui/viewmodel/DetailMahasiswaViewModel.kt
+++ app/src/main/java/com/example/roomsiswa/ui/viewmodel/DetailMahasiswaViewModel.kt
@@ -1,5 +1,11 @@
 package com.example.roomsiswa.ui.viewmodel
 
-data class DetailUiState(
+import com.example.roomsiswa.ui.viewmodel.MahasiswaViewModel.MahasiswaEvent
+import java.lang.Error
 
+data class DetailUiState(
+    val detailUiEvent: MahasiswaEvent = MahasiswaEvent(),
+    val isLoading: Boolean = false,
+    val isError: Boolean = false,
+    val errorMessage: String = ""
 )
